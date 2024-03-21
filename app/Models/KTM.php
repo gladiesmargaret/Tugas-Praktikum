594219a651ff27fe9f509d16ee0ff3a194f32f68
unknown <gladiesmargaret@gmail.com>
2024-03-22 01:20:43 +0700

    first init github

---
+++ app/Models/KTM.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KTM extends Model
+{
+    use HasFactory;
+    protected $table = 'ktm';
+
+    protected $primaryKey = 'id_ktm';
+
+    public function mahasiswa(){
+        return $this->belongsTo(Mahasiswa::class);
+    }    
+}
